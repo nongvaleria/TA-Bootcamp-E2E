ecd83ffe645306dafb774c2b4c6305eccc8e43d8
KIRYL NONG <kirylnong@KIRYLs-MBP.lan>
2022-11-30 19:11:04 +0100

    css_selectors_changes

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -5,8 +5,8 @@ Given('I am on the homepage', async () => {
 	await browser.url(`https://www.newegg.com/`)
 });
 Given('I close the promo banner if it appears', async () => {
-const $modal=await $('#modal-Website > div.modal-dialog.modal-dialog-centered > div > div > a > img');
-const $button=await $('#modal-Website > div.modal-dialog.modal-dialog-centered > div > button')
+const $modal=await $('[class="modal-Website-img"]');
+const $button=await $('[class="close"]')
 try {
     await $modal.toExist();
 }
@@ -15,27 +15,27 @@ catch {
 });
 
 When('As a user I entry the word Windows in the search bar', async () => {
-    const searchBar=await $('//*[@id="app"]/header/div[1]/div[1]/div[1]/div[4]/form/div/div[3]/button');
-    const searchInput=await $('//*[@id="app"]/header/div[1]/div[1]/div[1]/div[4]/form/div/div[1]/input')
+    const searchBar=await $('[class="ico ico-search"]');
+    const searchInput=await $('input[type="search"]')
     await searchBar.click()
     await searchInput.setValue('Windows');
 });
 
 When('I click the search button', async () => {
-    await $('//*[@id="app"]/header/div[1]/div[1]/div[1]/div[5]/form/div/div[2]/button').click()
+    await $('[class="ico ico-search"]').click()
 });
 
 Then('I see that at least one item appears', async () => {
-    const list = await $(`//*[@id="app"]/div[3]/section/div/div/div[2]/div/div/div/div[2]/div/div/div[2]`)
+    const list = await $(`[class="item-cell"]`)
     await expect(list).toBeExisting();
 });
 
 When("As a user I Open Today's Best Deals tab", async () => {
-await $('#trendingBanner_720202 > span').click()
+await $('[id="trendingBanner_720202"]').click()
 });
 
 When('I click on the Internet shop logo', async () => {
-await $('//*[@id="app"]/header/div[1]/div[1]/div[1]/div[2]/a/img').click()   
+await $('[class="header2021-logo-img"]').click()   
 });
 
 Then('I see that the main page is opened', async () => {
